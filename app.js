5eb1f31eb35618a84c1b9173dea75138790a80fa
Onur Simsek <onur.simsekk3@gmail.com>
2021-06-14 23:13:48 +0300

    code layout edited

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const colorButton = document.getElementById('colorBtn')
-const colorText = document.getElementById('colorText')
+const colorButton = document.getElementById('colorBtn');
+const colorText = document.getElementById('colorText');
 
 const charArray = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","F"];
 
